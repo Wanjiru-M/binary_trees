5facb89330927056b84328ead7fcef2c4ff19f11
wanjirumephis <wanjirunduhi@gmail.com>
2023-08-01 06:41:15 -0700

    update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,15 +8,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t height_left, height_right;
+size_t height_left, height_right;
 
-    if (tree == NULL)
-        return (0);
+if (tree == NULL)
+return (0);
 
-    height_left = binary_tree_height(tree->left);
-    height_right = binary_tree_height(tree->right);
+height_left = binary_tree_height(tree->left);
+height_right = binary_tree_height(tree->right);
 
-    return (1 + (height_left > height_right ? height_left : height_right));
+return (1 + (height_left > height_right ? height_left : height_right));
 }
 
 /**
@@ -27,23 +27,23 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    size_t height_left, height_right;
+size_t height_left, height_right;
 
-    if (tree == NULL)
-        return (0);
+if (tree == NULL)
+return (0);
 
-    height_left = binary_tree_height(tree->left);
-    height_right = binary_tree_height(tree->right);
+height_left = binary_tree_height(tree->left);
+height_right = binary_tree_height(tree->right);
 
-    if (height_left == height_right)
-    {
-        if (height_left == 0)
-            return (1);
+if (height_left == height_right)
+{
+if (height_left == 0)
+return (1);
 
-        return (binary_tree_is_perfect(tree->left) &&
-                binary_tree_is_perfect(tree->right));
-    }
+return (binary_tree_is_perfect(tree->left) &&
+binary_tree_is_perfect(tree->right));
+}
 
-    return (0);
+return (0);
 }
 
