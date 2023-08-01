a22330861aa47b753ade86bb98fe94b6b5ee4378
wanjirumephis <wanjirunduhi@gmail.com>
2023-08-01 05:37:01 -0700

    updtae

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -9,13 +9,13 @@
  */
 void binary_tree_postorder(const binary_tree_t *root, void (*func)(int))
 {
-    if (root == NULL || func == NULL)
-    {
-        return;
-    }
+if (root == NULL || func == NULL)
+{
+return;
+}
 
-    binary_tree_postorder(root->left, func);
-    binary_tree_postorder(root->right, func);
-    func(root->n);
+binary_tree_postorder(root->left, func);
+binary_tree_postorder(root->right, func);
+func(root->n);
 }
 
