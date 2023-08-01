a7dff8a979d3501dd1aee12bf70ea69e68e8eaed
wanjirumephis <wanjirunduhi@gmail.com>
2023-08-01 04:53:20 -0700

    update

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,14 +11,14 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-    if (!new_node)
-        return NULL;
+	binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
 
-    memset(new_node, 0, sizeof(binary_tree_t));
-    new_node->n = value;
-    new_node->parent = parent;
+	memset(new_node, 0, sizeof(binary_tree_t));
+	new_node->n = value;
+	new_node->parent = parent;
 
-    return new_node;
+	return (new_node);
 }
 
