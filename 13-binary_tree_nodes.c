c87145264b5d72bc675b04dfce0e0e0bb047a31f
wanjirumephis <wanjirunduhi@gmail.com>
2023-08-01 06:14:29 -0700

    update

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,16 +9,16 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
+if (tree == NULL)
+{
+return (0);
+}
 
-    if (tree->left || tree->right)
-    {
-        return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
-    }
+if (tree->left || tree->right)
+{
+return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
 
-    return (0);
+return (0);
 }
 
